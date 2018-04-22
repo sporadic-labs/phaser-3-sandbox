0eb1eaf969a43d7f5a074de5ab7594edf1c28970
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-21 22:41:47 -0500

    Add sprite variant made with component pattern

--- src/js/scenes/test-scene.js
+++ src/js/scenes/test-scene.js
@@ -1,16 +1,26 @@
 import Phaser from "phaser";
 import RotatingSprite from "../game-objects/rotating-sprite";
+import RotatingSpriteAsComponent from "../game-objects/rotating-sprite-as-component";
 
 export default class Scene extends Phaser.Scene {
   create() {
     this.sprite = new RotatingSprite(this, 100, 100, "ship");
     this.sprite.setScale(2);
+
+    this.spriteContainer = new RotatingSpriteAsComponent(this, 300, 100, "assets", "ship");
+    this.spriteContainer.sprite.setScale(2);
   }
 
   update(time, delta) {
-    if (time > 5000 && this.sprite) {
-      this.sprite.destroy();
-      this.sprite = null;
+    if (time > 5000) {
+      if (this.sprite) {
+        this.sprite.destroy();
+        this.sprite = null;
+      }
+      if (this.spriteContainer) {
+        this.spriteContainer.destroy();
+        this.spriteContainer = null;
+      }
     }
   }
 }
