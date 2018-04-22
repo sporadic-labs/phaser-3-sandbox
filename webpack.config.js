a684758a7906ef0af19f919dc5d862a91825b5f6
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-21 22:03:21 -0500

    Update webpack config to match v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,72 @@
 /* eslint-env node */
+
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
 
-module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: "./js/main.js",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "main.js"
-  },
-  module: {
-    rules: [
-      { test: /\.(js|jsx)$/, use: "babel-loader", exclude: /node_modules/ },
+module.exports = function(env, argv) {
+  const isDev = argv.mode !== "development";
 
-      {
-        test: /\.(scss|sass)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+  return {
+    mode: "development",
+    context: path.resolve(__dirname, "src"),
+    entry: "./js/main.js",
+    cache: true,
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "main.js"
+    },
+    module: {
+      rules: [
+        { test: /\.(js|jsx)$/, use: "babel-loader", exclude: /node_modules/ },
+
+        {
+          test: /\.(scss|sass)$/,
           use: [
+            isDev
+              ? { loader: "style-loader", options: { sourceMap: true } }
+              : MiniCssExtractPlugin.loader,
             { loader: "css-loader", options: { sourceMap: true } },
             { loader: "sass-loader", options: { sourceMap: true } }
           ]
-        })
-      },
+        },
 
-      //   Ensure that urls in scss are loaded correctly
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]"
+        //   Ensure that urls in scss are loaded correctly
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]"
+            }
           }
-        }
-      },
+        },
 
-      // Allow shaders to be loaded via glslify
-      { test: /\.(glsl|frag|vert)$/, use: ["raw-loader", "glslify"] }
-    ]
-  },
-  plugins: [
-    new HTMLWebpackPlugin({ template: "./index.html" }),
-    new ExtractTextPlugin({ filename: "[name].[contenthash].css", allChunks: true }),
-    // Instead of using imports & file loader for Phaser assets, just copy over all resources
-    new CopyWebpackPlugin([
-      {
-        from: "./resources",
-        to: "resources/",
-        ignore: ["audio/audacity/**/*", "atlases/*-frames/**/*"]
-      }
-    ]),
-    // Not sure why Phaser 3 made the decision to rely on globals, but so be it:
-    new webpack.DefinePlugin({ WEBGL_RENDERER: true, CANVAS_RENDERER: true })
-  ],
-  devtool: "source-map"
+        // Allow shaders to be loaded via glslify
+        { test: /\.(glsl|frag|vert)$/, use: ["raw-loader", "glslify"] }
+      ]
+    },
+    plugins: [
+      new HTMLWebpackPlugin({ template: "./index.html" }),
+
+      new MiniCssExtractPlugin({ filename: "[name].[hash].css", chunkFilename: "[id].[hash].css" }),
+
+      // Instead of using imports & file loader for Phaser assets, just copy over all resources
+      new CopyWebpackPlugin([
+        {
+          from: "./resources",
+          to: "resources/",
+          ignore: [
+            /* Nothing to ignore yet, but soon... */
+          ]
+        }
+      ]),
+
+      // Not sure why Phaser 3 made the decision to rely on globals, but so be it:
+      new webpack.DefinePlugin({ WEBGL_RENDERER: true, CANVAS_RENDERER: true, PRODUCTION: !isDev })
+    ],
+    devtool: isDev ? "eval-source-map" : "source-map"
+  };
 };
